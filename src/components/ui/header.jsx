d59dba5539edfb62304389b38c3130c15772d064
Kalevitutar <tanyamthomas@yahoo.com>
2025-01-27 15:44:06 -0500

    trying to see if this branch works

---
+++ src/components/ui/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+// import headerImage from "@public.headerImage.jpg";
+
+export default function Header() {
+    return (
+        <div>
+            {/* <div className="absolute -z-10 inset -0">
+            <Image
+            alt="description"
+            src={headerImage}
+            fill
+            style={{objectFit: "cover"}}
+            />
+        </div>
+        <Link href="/">Landing</Link> */}
+        </div>
+    );
+}
