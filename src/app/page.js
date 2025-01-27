e4f42e799bdd9d9a1fc7762a0eca21c721af0369
ana <ana@anniecannons.com>
2025-01-27 13:59:07 -0800

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,1 @@
 
-import styles from "./page.module.css";
-
-
-import { Button, VStack } from "@chakra-ui/react";
-
-export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
-}
-
