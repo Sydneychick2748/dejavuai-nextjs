76f83c0e1cdd75742ef5f73a5def8ac9b88e148b
ana <ana@anniecannons.com>
2025-01-31 10:30:07 -0800

    added some npx chakra components for the dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,21 +1,5 @@
-'use client';
-
-
-import { Avatar, } from '@chakra-ui/react';
-import { Box } from '@chakra-ui/react';
-import {Button} from "@/components/ui/button";
-import { Avatar } from '@chakra-ui/react';
+"use client";
 
 export default function Dashboard() {
-    return (
-
-        <>
-        {/* <Avatar size="2xl" bg="gray.200" /> */}
-<Button>
-    click me 
-</Button>
-<Box bg="gray.200" p={4}>This is a box</Box>;
-<Avatar size="2xl" bg="gray.200" />
-        </>
-    )
+  return <></>;
 }
