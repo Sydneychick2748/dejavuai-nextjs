71470b37a743d75d2ba92a56e149772ae1f6c405
Tberry <Tberry@anniecannons.com>
2025-02-05 20:57:45 -0600

    styling the welcome page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,36 @@
 import { Box, Button, Heading, Text, Image, VStack } from "@chakra-ui/react";
 
+const styles = {
+  headingWelcome: {
+    fontSize: "4.5rem",
+    color: "#00264A",
+    fontFamily: "'Poppins', sans-serif",
+    fontWeight: "100",
+    textAlign: "left", // Align text to the right
+    marginBottom: "20px",
+    marginRight: "auto", // Pushes it to the right if inside a flex/grid container
+  },
+  imageStyle: {
+    width: "100%", // Adjust width
+    height: "100vh", // Maintain aspect ratio
+    position: "relative", // Allows free movement
+    top: "50px", // Move down from the top
+    left: "100px", // Move right from the left
+  },
+  textStyle: {
+    fontSize: "30px",
+    color: "#00264A",
+    fontWeight: "500", // Optional: Adjust thickness
+    position: "relative", // Allows precise movement
+    top: "-30px", // Move up/down (lower value moves it up)
+    left: "300px", // Move left/right (higher value moves it right)
+  },
+};
+
+
+
+
+
 export default function Home() {
   return (
     <Box
@@ -11,18 +42,28 @@ export default function Home() {
       bg="gray.50"
       bgImage="url('/images/background/DejaVuBackground.png')"
     >
-
       <VStack spacing={6} textAlign="center">
         {/* Titles */}
-        <Heading as="h1" size="6xl" color="#00264A">
+        <Heading as="h1" style={styles.headingWelcome}>
           Welcome to
         </Heading>
 
-        <Heading as="h1" fontSize="120px" color="#00264A">
+        {/* Welcome logo section Image Section */}
+        <Box>
+          <Image
+            src="/images/logos/DejavuaiLogoTransparent.png"
+            alt="Company Logo"
+            styles={styles.imageStyle} 
+          />
+        </Box>
+
+        {/* <Heading as="h1" style ={styles.headingDejavu}>
           DejaVuAI
-        </Heading>
-        <Text fontSize="20px" color="#00264A">Trace The Untraceable</Text>
-        {/* Image Section */}
+        </Heading> */}
+        <Text style={styles.textStyle} >
+          Trace The Untraceable
+        </Text>
+        {/* Image Section
         <Box w="40%" display="flex" justifyContent="center" alignItems="center">
           <Image
             src="/images/logos/dvai-icon.png"
@@ -30,7 +71,7 @@ export default function Home() {
             // width="0"
             // height="150px"
           />
-        </Box>
+        </Box> */}
         {/* Buttons */}
         <VStack spacing={4}>
           <Button>Log In</Button>
