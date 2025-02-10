c4ca417918b6e285c18d16a7f62f90c0f913c0c5
Kalevitutar <tanyamthomas@yahoo.com>
2025-02-10 16:54:02 -0500

    working on headers

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,28 @@
-import "./globals.css";
+"use client"
 
+import "./globals.css";
 import { Provider } from "@/components/ui/provider"
-import Header from "@/components/ui/header1"
-import Footer from "@/components/ui/footer1"
-export const metadata = {
-  title: "DejaVuAI",
-  description: "amazing site right here",
-};
+import Header1 from "@/components/ui/header1"
+import Header2 from "@/components/ui/header2"
+import Footer1 from "@/components/ui/footer1"
+import { useRouter } from 'next/navigation'
+
+// export const metadata = {
+//   title: "DejaVuAI",
+//   description: "amazing site right here",
+// };
+
+
 export default function RootLayout({ children }) {
+  const router = useRouter()
   return (
     <html lang= "en" suppressHydrationWarning>
       <body  suppressHydrationWarning>
         <Provider>
-        <Header />
+        {/* {router.pathname === 'localhost:3000' ? <Header2 /> : <Header1 />} */}
+        {window.location.origin === 'http://localhost:3000/create-account' || window.location.origin === 'http://localhost3000/login' || window.location.origin === 'http://localhost3000/profile' || window.location.origin === 'http://localhost3000/contact' || window.location.origin === 'http://localhost3000/dashboard' || window.location.origin === 'http://localhost3000/help' ? <Header2 /> : <Header1 />}
         {children}
-        <Footer />
+        <Footer1 />
         </Provider>
       </body>
     </html>
