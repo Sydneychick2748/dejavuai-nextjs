7cfe503a8c9cc25e57859954e738926f0127d3bc
Kalevitutar <tanyamthomas@yahoo.com>
2025-02-10 17:54:57 -0500

    Solved header issue re different pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Header1 from "@/components/ui/header1"
 import Header2 from "@/components/ui/header2"
 import Footer1 from "@/components/ui/footer1"
 import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 // export const metadata = {
 //   title: "DejaVuAI",
@@ -15,12 +16,14 @@ import { useRouter } from 'next/navigation'
 
 export default function RootLayout({ children }) {
   const router = useRouter()
+  const pathname = usePathname()
+  console.log('pathname is ', pathname)
   return (
     <html lang= "en" suppressHydrationWarning>
       <body  suppressHydrationWarning>
         <Provider>
         {/* {router.pathname === 'localhost:3000' ? <Header2 /> : <Header1 />} */}
-        {window.location.origin === 'http://localhost:3000/create-account' || window.location.origin === 'http://localhost3000/login' || window.location.origin === 'http://localhost3000/profile' || window.location.origin === 'http://localhost3000/contact' || window.location.origin === 'http://localhost3000/dashboard' || window.location.origin === 'http://localhost3000/help' ? <Header2 /> : <Header1 />}
+        {pathname === 'accounts/create-account' || pathname === 'accounts/login' || pathname === 'accounts/profile' || pathname === '/contact' || pathname === '/dashboard/upload-files' || pathname === '/help' || pathname === '/dashboard' ? <Header2 /> : <Header1 />}
         {children}
         <Footer1 />
         </Provider>
