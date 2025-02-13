29bd5a5474d908b91b5bb5b4982caedf07ff1d5b
Kalevitutar <tanyamthomas@yahoo.com>
2025-02-13 18:01:03 -0500

    work on headers and footers

--- src/components/ui/footer1.jsx
+++ src/components/ui/footer1.jsx
@@ -1,8 +1,8 @@
 import { Box, Heading } from "@chakra-ui/react";
 
-export default function Footer() {
+export default function Footer1() {
     return (
-        <Box as="footer" className="headerFooter1-gradient">
+        <Box as="footer" className="headerFooter1-gradient footer">
             <Heading as="h1" size="lg">
                 Ana is an awesome coder!!!!
             </Heading>
