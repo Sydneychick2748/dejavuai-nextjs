5dc5e289f05ca13a425fe30c05653fc13d76ef9b
Tberry <Tberry@anniecannons.com>
2025-02-21 11:14:21 -0600

    adding searchParams code from tutar

--- src/app/dashboard/search-params/searchParams.js
+++ src/app/dashboard/search-params/searchParams.js
@@ -1,12 +1,12 @@
 "use client";
-
-import { Box, Text } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Box, Text, Button } from "@chakra-ui/react";
 import Simple from "@/components/searchParams/simple/simple";
 import SavedSettings from "@/components/searchParams/saved-settings/saved-settings";
 import Advanced from "@/components/searchParams/advanced/advanced";
-import "./searchParams.css";
-
-export default function SearchParams() {
+import styles from "./searchParams.css";
+export default function SettingsPanel() {
+  const [activeTab, setActiveTab] = useState("Simple");
   return (
     <div className="settings">
       <Box className="headerBox">
@@ -19,17 +19,36 @@ export default function SearchParams() {
           </Text>
         </Box>
       </Box>
-      <div className="simple">
-        <Text>Simple</Text>
-        <Simple />
-      </div>
-      <div className="savedSettings">
-        <Text>Saved Settings</Text>
-        <SavedSettings />
-      </div>
-      <div className="advanced">
-        <Text>Advanced</Text>
-        <Advanced />
+      <div className="container2">
+      {/* Left Sidebar */}
+        <div className="sidebar">
+          <ul>
+            <li
+              className={activeTab === "Simple" ? "active" : ""}
+              onClick={() => setActiveTab("Simple")}
+            >
+              Simple
+            </li>
+            <li
+              className={activeTab === "SavedSettings" ? "active" : ""}
+              onClick={() => setActiveTab("SavedSettings")}
+            >
+              Saved Settings
+            </li>
+            <li
+              className={activeTab === "Advanced" ? "active" : ""}
+              onClick={() => setActiveTab("Advanced")}
+            >
+              Advanced
+            </li>
+          </ul>
+        </div>
+        {/* Right Content Box */}
+        <div className="content2">
+          {activeTab === "Simple" && <Simple />}
+          {activeTab === "SavedSettings" && <SavedSettings />}
+          {activeTab === "Advanced" && <Advanced />}
+        </div>
       </div>
     </div>
   );
